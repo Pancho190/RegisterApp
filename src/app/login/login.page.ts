61f9808d8aef068eaed602812cb29190790444c1
FranciscoSilva <franc.silvaj@duocuc.cl>
2024-11-07 20:33:39 -0300

    Update

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  login = {
+    Usuario: '',
+    Password: ''
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+
+  ingresar() {
+    console.log('Usuario:', this.login.Usuario);
+    console.log('Password:', this.login.Password);
   }
-
 }
