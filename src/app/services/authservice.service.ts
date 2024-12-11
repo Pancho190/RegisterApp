88dbd23b74554017708576777be843fa22df3367
CristianBrionesINf <c.briones@duocuc.cl>
2024-12-10 23:36:20 -0300

    cambios quitados

--- src/app/services/authservice.service.ts
+++ src/app/services/authservice.service.ts
@@ -1,28 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-import { Observable, retry } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-
 export class AuthserviceService {
-  dbUrl = 'https://e6771af7-3036-4e88-87a0-ffe6c7b75719-00-1q7ws21v90607.kirk.replit.dev/'; // Endpoint JSON Server para usuarios
-
-  httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
-    })
-  };
-
+  private dbUrl = 'http://localhost:3000/usuarios'; // Endpoint JSON Server para usuarios
 
   constructor(private http: HttpClient) {}
-
-  getUserByUsername(username: string): Observable<any>{
-    return this.http.get(`${this.dbUrl}/usuarios?username=${username}`, this.httpOptions);
-  }
-
 
   // Método para obtener la lista de usuarios
   getUsuarios(): Observable<any> {
@@ -31,7 +17,7 @@ export class AuthserviceService {
 
   // Método para validar usuario y contraseña
   validateUser(username: string, password: string): Observable<any> {
-    return new Observable((observer: { next: (arg0: any) => void; complete: () => void; }) => {
+    return new Observable((observer) => {
       this.getUsuarios().subscribe((data: any[]) => {
         const user = data.find(
           (item: any) =>
