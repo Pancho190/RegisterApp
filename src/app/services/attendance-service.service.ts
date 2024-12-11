92d31e69f443aa4ce813a4d0caeabe6df27d6ab2
CristianBrionesINf <c.briones@duocuc.cl>
2024-12-10 23:23:35 -0300

    update

--- src/app/services/attendance-service.service.ts
+++ src/app/services/attendance-service.service.ts
@@ -1,27 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class AttendanceService {
-  apiUrl = 'https://e6771af7-3036-4e88-87a0-ffe6c7b75719-00-1q7ws21v90607.kirk.replit.dev/'; // URL del json-server
-
-  HttpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
-    })
-  }
+  private apiUrl = 'http://localhost:3000'; // URL del json-server
 
   constructor(private http: HttpClient) {}
-
-  getUsersByUsername(username: string): Observable<any> {
-    return this.http.get(`${this.apiUrl}/usuarios?username=${username}`, {headers: this.HttpOptions.headers});
-  }
 
   // Obtener usuarios
   getUsers(): Observable<any> {
